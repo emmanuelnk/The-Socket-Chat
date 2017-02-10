f209e0f50bac56c1667581cdee982013b0166f21
Emmanuel Kyeyune <emmakyeyune@gmail.com>
2017-02-10 10:55:00 +0100

    Added Socket receive message and broadcast in server

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,12 @@
 var socket = io();
 socket.on("connect", function (){
 	console.log("Connected to socket.io console!");
-})
+});
+
+socket.on("message", function (message) {
+console.log("Message:")	
+console.log(message.text);
+console.log("Message Time:")	
+console.log(message.msgTime);
+
+});
--- server.js
+++ server.js
@@ -3,13 +3,24 @@ var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname + "/public"));
 
-io.on("connection", function () {
+io.on("connection", function(socket) {
 	console.log("User connected via Socket.io!");
-})
+
+	socket.on("message", function(message) {
+		console.log("Message received: " + message.text);
+		socket.broadcast.emit("message", message);
+	});
+
+	socket.emit("message", {
+		text: "Welcome to the Chat App!",
+		msgTime: moment().format("YYYY-MM-DD hh:mm:ss")
+	});
+});
 
 http.listen(PORT, function() {
 	console.log("Server started at port:" + PORT + "!");
-}); 
+});
