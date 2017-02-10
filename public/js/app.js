a1dde8371198992f8d9ace1fa1e5971d39481dfa
Emmanuel Kyeyune <emmakyeyune@gmail.com>
2017-02-10 13:06:26 +0100

    Added Form elements for Socket comm

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,18 @@ console.log("Message Time:")
 console.log(message.msgTime);
 
 });
+
+
+// submit new message
+
+var $form = jQuery("#message-form");
+$form.on("submit", function (event) {
+	event.preventDefault();
+	socket.emit("message", {
+		text:$form.find("input[name=message ]").val(),
+		msgTime:moment().format("YYYY-MM-DD hh:mm:ss")
+	});
+	
+	$form.find("input[name=message ]").val("").focus();
+
+});
