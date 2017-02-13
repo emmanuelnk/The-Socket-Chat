38d81add1e590edefa0baeb1df54e6c5f22823fe
Emmanuel Kyeyune <emmakyeyune@gmail.com>
2017-02-13 17:42:34 +0100

    Added Bootstrap styling to chat app

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ var now = moment();
 var userName = getQueryVariable("name") || "Anonymous";
 var userRoom = getQueryVariable("room") || "Anonymous";
 var randColor = getRandomColor();
+var lastMessageSentTime;
 
 socket.on("connect", function() {
 	console.log("Connected to socket.io console!");
@@ -13,23 +14,44 @@ socket.on("connect", function() {
 		userColor: randColor,
 		userRoom: userRoom
 	});
+
+	lastMessageSentTime = parseInt(now.utc().format('x'));
+	var firstTimestamp = moment().parseZone(now.format()).local().format("Do-MMM-YYYY h:mm a");
+	$(".messages").append("<p style=\"text-align:center;color: grey;\"><b>" + firstTimestamp + "</b></p>");
+	console.log(lastMessageSentTime);
 });
 
 socket.on("message", function(message) {
 	//convert time from UTC to local timezone using local()
-	var timestamp = moment.parseZone(message.msgTime).local().format("Do-MMM-YYYY h:mma");
-	$(".messages").append("<p><b>" + timestamp + "<span style=\"color:" + message.userColor + ";\" > [" + message.userName + "]</span>:</b>" + message.text + "</p>");
+	if (parseInt(moment().parseZone(message.msgTime).utc().format("x")) - lastMessageSentTime > 120000) {
+		var fullTimestamp = moment().parseZone(message.msgTime).local().format("Do-MMM-YYYY h:mm a");
+		$(".messages").append("<p style=\"text-align:center;color:grey;\"><b>" + fullTimestamp + "</b></p>");
+	}
 
+	var smallTimestamp = moment().parseZone(message.msgTime).local().format("h:mm:ss a");
+	console.log(smallTimestamp);
+	$(".messages").append("<p><b style=\"color:grey;\">" + smallTimestamp + "</b><b><span style=\"color:" + message.userColor + ";\" > [" + message.userName + "]</span>:</b>" + message.text + "</p>");
+
+	lastMessageSentTime = moment().parseZone(message.msgTime).utc().format('x');
 });
 
 socket.on("userJoined", function(userJoined) {
 	//convert time from UTC to local timezone using local()
-	var timestamp = moment.parseZone(userJoined.joinTime).local().format("Do-MMM-YYYY h:mma");
+	var timestamp = moment.parseZone(userJoined.joinTime).local().format("Do-MMM-YYYY h:mm:ss a");
 	$(".messages").append("<p style=\"color:" + userJoined.userColor + ";\">" + userJoined.userName + " just joined the <b>" + userJoined.userRoom + "</b> chat!</p>");
 
 });
 
-$("#room-title").text(userRoom);
+socket.on("userList", function(users) {
+	var list = users.userlist;
+	$("#currentUsers").empty();
+	$("#currentUsers").append("<h4><b>users:</b></h4>");
+	list.forEach(function (el) {
+		$("#currentUsers").append("<p style=\"color:" + el.userColor + ";\">" + el.userName + "</p>");
+	});
+});
+
+$("#room-title").text("chatroom: " + userRoom);
 // submit new message
 
 $("#message-form").on("submit", function(event) {
@@ -48,9 +70,25 @@ $("#message-form").on("submit", function(event) {
 
 function getRandomColor() {
 	var letters = '0123456789ABCDEF';
+	var lettersBright = '456789ABCDEF';
+
 	var color = '#';
-	for (var i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
+
+	var count = 0;
+	switch (count) {
+		case 0:
+			color += lettersBright[Math.floor(Math.random() * 12)];
+		case 1:
+			color += letters[Math.floor(Math.random() * 16)];
+		case 2:
+			color += lettersBright[Math.floor(Math.random() * 12)];
+		case 3:
+			color += letters[Math.floor(Math.random() * 16)];
+		case 4:
+			color += lettersBright[Math.floor(Math.random() * 12)];
+		case 5:
+			color += letters[Math.floor(Math.random() * 16)];
+			break;
 	}
 	return color;
 }
--- server.js
+++ server.js
@@ -35,11 +35,35 @@ function sendCurrentUsers(socket) {
 	});
 }
 
+function populateCurrentUsersList(socket,room) {
+	var info = clientInfo[socket.id];
+	var users = {
+		userlist:[]
+	}
+
+	if (typeof info === "undefined") {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function(socketId) {
+		var userinfo = clientInfo[socketId];
+		if (info.userRoom === userinfo.userRoom) {
+
+			users.userlist.push({
+				userName:userinfo.userName,
+				userColor:userinfo.userColor
+			});
+		}
+	});
+	io.to(room).emit("userList", users);
+}
+
 io.on("connection", function(socket) {
 	console.log("User connected via Socket.io!");
 
 	socket.on("disconnect", function() {
 		if (typeof clientInfo[socket.id] !== "undefined") {
+			populateCurrentUsersList(socket,clientInfo[socket.id].userRoom);
 			socket.leave(clientInfo[socket.id]);
 			io.to(clientInfo[socket.id].userRoom).emit("message", {
 				text: clientInfo[socket.id].userName + " left the chat room!",
@@ -47,8 +71,10 @@ io.on("connection", function(socket) {
 				userName: "Server",
 				userColor: "grey"
 			});
+			
 			delete clientInfo[socket.id];
 		}
+
 	});
 
 	socket.on("message", function(message) {
@@ -65,6 +91,8 @@ io.on("connection", function(socket) {
 		console.log(userJoined.joinTime + ": " + userJoined.userName + " just joined " + userJoined.userRoom + " chat room!");
 		socket.join(userJoined.userRoom);
 		socket.broadcast.to(userJoined.userRoom).emit("userJoined", userJoined); // everybody including sender
+		sendCurrentUsers(socket);
+		populateCurrentUsersList(socket,userJoined.userRoom);
 	});
 
 	socket.emit("message", {
